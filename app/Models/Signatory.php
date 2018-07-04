8d9070019da3d943dd8d92aeaa3c707260d99955
Benoît Ripoche <ripoche.b@gmail.com>
2018-07-04 11:26:51 +0000

    add signatories model

---
+++ app/Models/Signatory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Signatory extends Model
+{
+    protected $fillable = [
+        'id_rds',
+        'first_name',
+        'last_name',
+        'email',
+        'company',
+        'has_signed',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+}
+
+
---
+++ database/migrations/2018_07_04_100727_create_signatories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSignatoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signatories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_rds');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('company');
+            $table->boolean('has_signed')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('signatories');
+    }
+}
