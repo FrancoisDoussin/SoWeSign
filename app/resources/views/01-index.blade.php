bd6e8ad3a7e52e9074777401c5ad54bc5e163506
François Doussin <doussin.f@gmail.com>
2018-07-11 14:21:14 +0200

    Revert "Feature/add route and view"

--- app/resources/views/01-index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.base')
-
-@section('content')
-    <form action="{{ route('parse-pdf') }}" enctype="multipart/form-data" method="POST">
-        @csrf
-        <input name="pdf" type="file">
-        <button type="submit">Télécharger</button>
-    </form>
-@endsection
--- node-server/src/index.js
+++
@@ -1,5 +0,0 @@
-import Server from "../build/server"
-
-const server = new Server()
-
-server.run()
