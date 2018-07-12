06e19e44ef5a0d6e5d60f48beb65c45a410688d6
Benoît Ripoche <ripoche.b@gmail.com>
2018-07-12 15:49:14 +0200

    fix

--- app/resources/views/06-manage.blade.php
+++ app/resources/views/06-manage.blade.php
@@ -3,7 +3,7 @@
 @section('content')
    Bienvenue {{ $rds->admin_first_name }} {{ $rds->admin_last_name }}
    <br>
-   voir l'état des signatures : {{config('app.url'). '/state/' .$rds->url_two_hash}}
+   <a href="{{ config('app.url'). '/state/' .$rds->url_two_hash }}">voir l'état des signatures</a>
    <br>
-   <a href="{{config('app.url'). '/download/' .$rds->url_three_hash}}">télécharger le pdf </a>
+   <a href="{{config('app.url'). '/download/' .$rds->url_three_hash}}">télécharger le pdf</a>
 @endsection
